e42ebde5b4c7df0965684bd89f7f38fdd1cb5e44
Saif <developbysaif@gmail.com>
2026-06-21 11:22:30 +0500

    first commit

--- number guess/guess.py
+++ number guess/guess.py
@@ -1,13 +1,13 @@
 print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 10.")
-
 secret = 10
 
 num = int(input("take a guess:"))
-
-if num == secret:
-    print("Good job! You guessed my number!")
-
+for i in range(3):
+    num = int(input("take a guess:"))   
+    if num == secret:
+        print("Good job! You guessed my number!")
+        break
+    num = int(input("take a guess:"))   
 else:
     print("Sorry, the number I was thinking of was", secret)    
 
